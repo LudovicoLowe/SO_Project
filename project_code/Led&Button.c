09694747b6a07d00fae251b65f2881054f5241bb
LudovicoLowe <lowe.l@hotmail.com>
2018-06-09 16:32:23 +0200

    cooments update

--- project_code/Led&Button.c
+++ project_code/Led&Button.c
@@ -3,6 +3,8 @@
  
 #define delay 1000
 
+//suppongo il led collegato al pin D13, e il bottone al pin D2
+
 int main (void) {
 	DDRB = 0b00100000; // (PB5=D13)  Setto l'uscita PB5 come uscita
 	DDRD = 0b00000000; // (PD2=D2)  Setto l'uscita PD2 come entrata
