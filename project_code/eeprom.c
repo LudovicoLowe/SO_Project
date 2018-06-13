96b43f664b39167060bb6d7dd62df7f90bef0754
LudovicoLowe <lowe.l@hotmail.com>
2018-06-14 01:06:37 +0200

    minor polish

--- project_code/eeprom.c
+++ project_code/eeprom.c
@@ -4,22 +4,22 @@
 void EEPROM_init(void){
 }
 
-void EEPROM_read(void* dest_, const uint16_t src, uint16_t size){
-  uint8_t * dest=(uint8_t*)dest_;
-  uint16_t s=src;;
-  uint16_t s_end=src+size;
-  while(s<s_end){
+void EEPROM_read(void* dest, const uint16_t src, uint16_t size){
+  uint8_t * d=(uint8_t*)dest;
+  uint16_t s=src;
+  uint16_t end=src+size;
+  while(s<end){
     eeprom_busy_wait();
-    *dest=eeprom_read_byte((uint8_t*)s);
+    *d=eeprom_read_byte((uint8_t*)s);
     ++s;
-    ++dest;
+    ++d;
   }
 }
 
-void EEPROM_write(uint16_t dest, const void* src_,  uint16_t size){
-  const uint8_t * s=(uint8_t*)src_;
-  const uint8_t * s_end=s+size;
-  while(s<s_end){
+void EEPROM_write(uint16_t dest, const void* src,  uint16_t size){
+  const uint8_t * s=(uint8_t*)src;
+  const uint8_t * end=s+size;
+  while(s<end){
     eeprom_busy_wait();
     eeprom_write_byte((uint8_t*)dest, *s);
     ++s;
