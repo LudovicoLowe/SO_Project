247642bd52091634fd10a0f9478f0a64266e4881
Paolorollo <39830028+Paolorollo@users.noreply.github.com>
2018-06-15 12:28:49 +0200

    Added io.c and io.h files

---
+++ project_code/io.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdint.h>
+
+// initializes the digital io pins to be all input
+void DigIO_init(void);
+
+// returns the number of digital io pins
+uint8_t  DigIO_numChannels(void);
+
+typedef enum {
+  Input=0x0,
+  Output=0x1
+} PinDirection;
+
+uint8_t DigIO_setDirection(uint8_t pin, PinDirection dir);
+
+PinDirection DigIO_getDirection(uint8_t pin);
+
+void DigIO_setValue(uint8_t pin, uint8_t value);
+
+uint8_t DigIO_getValue(uint8_t pin);
