7ffbdf6b2405d63a741526e05e17378e521f46f2
Paolorollo <39830028+Paolorollo@users.noreply.github.com>
2018-06-16 16:03:14 +0200

    Add files via upload

---
+++ project_code/client/client_protocol.h
@@ -0,0 +1,34 @@
+#pragma once
+
+typedef struct LOG{
+  int n;
+  int temperature;
+  int humidity;
+} LOG;
+
+typedef enum {
+  Req=0x1,
+  Ans=0x2,
+} Type;
+
+typedef enum {
+  SetTimer=0x3,
+  LogRequest=0x4,
+} ReqType;
+
+typedef struct {
+  Type type;
+  ReqType req_type;
+  //timer set up
+} Request;
+
+typedef struct {
+  Type type;
+  LOG log;
+} Answere;
+
+// converts a well formed packet into a string in dest
+void Packet_serialize(char* dest, const void* h);
+
+// returns a newly allocated packet read from the buffer
+Type* Packet_deserialize(const char* buffer);
